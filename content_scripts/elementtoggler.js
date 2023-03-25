c6cca814c3e6ab68584262009d8cc418f01a2896
nader <nader796@gmail.com>
2023-03-25 18:03:39 -0400

    added selection box when hovering over elements

--- content_scripts/elementtoggler.js
+++ content_scripts/elementtoggler.js
@@ -9,7 +9,36 @@
     console.log("Element Toggler Enabled");
 
     function selectBtn() {
-        
+        // create box
+        var hoverBox = document.createElement("div");
+        hoverBox.style.position = "absolute";
+        hoverBox.style.background = "lightblue";
+        hoverBox.style.zIndex = "-9999"; // avoid blocking elements
+        // hoverBox.style.transition = "all 200ms ease-out"
+        // hoverBox.style.border = "5px solid red";
+        document.body.appendChild(hoverBox);
+
+        //repositions hoverbox to show selection
+        document.addEventListener("mouseover", function boxListener(e) {
+            if (e.target.id.indexOf('selector') !== -1
+                || e.target.tagName === 'BODY'
+                || e.target.tagName === 'HTML')
+                return;
+
+            const target = e.target;
+            const targetOffset = target.getBoundingClientRect();
+            const targetHeight = targetOffset.height;
+            const targetWidth = targetOffset.width;
+
+            const boxBorder = 5;
+            hoverBox.style.width = targetWidth + boxBorder * 2 + "px";
+            hoverBox.style.height = targetHeight + boxBorder * 2 + "px";
+            
+            // need scrollX and scrollY to account for scrolling
+            hoverBox.style.top = targetOffset.top + window.scrollY - boxBorder + "px";
+            hoverBox.style.left = targetOffset.left + window.scrollX - boxBorder + "px";
+
+        });
     }
 
     function toggleBtn() {
